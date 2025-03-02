bf4eccb5e2fdcec81d0ab28a1aaaa22f5912dba6
PhatMap <phatmap234@gmail.com>
2025-03-02 22:16:26 +0700

    layout menu and slider

---
+++ src/components/Slider.tsx
@@ -0,0 +1,9 @@
+const Slider = () => {
+    return (
+        <section className="w-full h-full">
+            <h1 className="font-semibold">Slider</h1>
+        </section>
+    )
+}
+
+export default Slider
